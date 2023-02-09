e631e46514a4c4417f4e7956363b6d13c4efa862
LeadConsult <106965102+LeadConsult@users.noreply.github.com>
2023-02-09 01:22:35 +0100

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Exports to-do list information of all employees to JSON format."""
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
 
